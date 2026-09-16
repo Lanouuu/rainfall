c8563cfdb50daee9a80a251130de0eefd78c26e0
ade-fran <ade-fran@f6r12s14.paris.42.school>
2026-09-16 16:53:23 +0200

    up

---
+++ level8/source.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *auth;
+char *service;
+
+int main(void)
+{
+    char buffer[128];
+
+    while (1)
+    {
+        printf("%p, %p \n", auth, service);
+
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+            break;
+
+        if (strncmp(buffer, "auth ", 5) == 0)
+        {
+            auth = malloc(4);
+            *auth = 0;
+
+            if (strlen(buffer + 5) <= 30)
+                strcpy(auth, buffer + 5);
+        }
+
+        if (strncmp(buffer, "reset", 5) == 0)
+        {
+            free(auth);
+        }
+
+        if (strncmp(buffer, "service", 7) == 0)
+        {
+            service = strdup(buffer + 7);
+        }
+
+        if (strncmp(buffer, "login", 5) == 0)
+        {
+            if (*(auth + 0x20) != 0)
+                system("/bin/sh");
+            else
+                fwrite("Password:\n", 1, 10, stdout);
+        }
+    }
+
+    return 0;
+}
