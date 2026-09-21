bf12d15d6f4e8591a1f57974fe8108057e6cd5bb
cmaubert <cmaubert@f6r11s14.paris.42.school>
2026-09-21 11:35:22 +0200

    petites corrections de syntaxes dans le source du level6

--- level6/source.c
+++ level6/source.c
@@ -1,12 +1,10 @@
 void n(void)
-
 {
   system("/bin/cat /home/user/level7/.pass");
   return;
 }
 
 void m(void *param_1,int param_2,char *param_3,int param_4,int param_5)
-
 {
   puts("Nope");
   return;
